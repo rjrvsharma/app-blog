8dc8f94b45233da62342a10d5538fa76a995a241
Ravishankar Vishwakarma <rjrvsharma@gmail.com>
2022-12-27 18:04:03 +0530

    final ui

--- src/App.js
+++ src/App.js
@@ -3,13 +3,44 @@ import Single from "./pages/single/Single";
 import TopBar from "./Components/topbar/TopBar";
 import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
+import {
+  BrowserRouter as Router,
+
+  Route,
+ 
+  Routes
+} from "react-router-dom";
+import Singlepost from "./Components/singlepost/Singlepost";
 
 function App() {
+  const User = true;
   return (
-    <>
-    <TopBar/>
-    <Settings/>
-    </>
+    <Router>
+        <TopBar />
+       
+
+        <Routes>
+          <Route path="/" exact element={<Home/>}></Route> 
+
+          <Route path="/register" element={User ? <Home/> : <Register />} ></Route>
+
+          <Route path="/login" element={User ? <Home/> : <Login />} ></Route>
+
+          <Route path="/write" element={User ? <Write /> : <Register />} ></Route>
+
+          <Route path="/settings" element={User ? <Settings /> : <Register/>} ></Route>
+
+          <Route path="/Singlepost" element={<Singlepost/>} ></Route>
+
+          <Route path="/post/:postId" element={<Single />} ></Route> 
+
+          
+
+        </Routes>
+      </Router>
   );
 }
 
