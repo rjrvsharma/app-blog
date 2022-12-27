4be1144f1dc573ea09d8cea75bc593e87c6a0cac
Ravishankar Vishwakarma <rjrvsharma@gmail.com>
2022-12-27 18:15:43 +0530

    done

--- src/Components/topbar/TopBar.jsx
+++ src/Components/topbar/TopBar.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 import './topbar.css'
 
 function TopBar() {
+  const User = true;
   return (
     <div className='top'>
         <div className='topLeft'>
@@ -12,15 +14,42 @@ function TopBar() {
         </div>
         <div className='topCenter'>
             <ul className='topList'>
-              <li className='topListItem'>Home</li>  
-              <li className='topListItem'>About</li>  
-              <li className='topListItem'>Contact</li>  
-              <li className='topListItem'>Write</li>  
-              <li className='topListItem'>Logout</li>  
+              <li className='topListItem'>
+                <Link to="/" className='link'>Home</Link>
+              </li>  
+              <li className='topListItem'>
+              <Link to="/Singlepost" className='link'>About</Link>
+                </li>  
+              <li className='topListItem'>
+              <Link to="/settings" className='link'>Contact</Link>
+                </li>  
+              <li className='topListItem'>
+              <Link to="/Write" className='link'>Write</Link>
+                </li>  
+              <li className='topListItem'>
+                {User && "Logout"}
+                </li>  
             </ul>    
         </div>
         <div className='topRight'>
-            <img className='topImg' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGB78NUo7qlfhAYYX2d00oXcqefIbE1QKIPk1ko51cFloCSiF8-wlrpHjjoAIHepffZAw&usqp=CAU" alt="" />
+          {
+            User ? (
+             
+              <img className='topImg' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGB78NUo7qlfhAYYX2d00oXcqefIbE1QKIPk1ko51cFloCSiF8-wlrpHjjoAIHepffZAw&usqp=CAU" alt="" />
+
+            ) : (
+              <ul className='topList'>
+
+                <li className='topListItem'>
+                   <Link to="/Login" className='link'>LOGIN</Link>
+                </li>
+                <li className='topListItem'>
+                <Link className='link' to="/R egister">REGISTER</Link>
+               </li>
+             
+              </ul>
+            )
+          }
             <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
         </div>
     </div>
